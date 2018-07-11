a2eb6f1c6e5d9add74a19b5ae612dca3b6b0a18d
songam <songam@jfinfo.com>
2018-07-11 18:02:17 +0800

    add execute job

--- src/main/java/jf/App.java
+++ src/main/java/jf/App.java
@@ -1,6 +1,7 @@
 package jf;
 
 
+import org.apache.commons.cli.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -9,15 +10,10 @@ import org.pentaho.di.core.KettleEnvironment;
 import org.pentaho.di.core.util.EnvUtil;
 import org.pentaho.di.trans.Trans;
 import org.pentaho.di.trans.TransMeta;
+import org.pentaho.di.job.JobMeta;
+import org.pentaho.di.job.Job;
+
 import java.util.UUID;
-
-
-
-//import org.apache.commons.cli.CommandLineParser;
-//import org.apache.commons.cli.BasicParser;
-//import org.apache.commons.cli.Options;
-//import org.apache.commons.cli.CommandLine;
-
 
 
 /**
@@ -39,26 +35,50 @@ public class App
         System.out.println("start!");
 
 
+//        logger_info.info("s={}",args[0]);
+
         // Create a Parser
-//        CommandLineParser parser = new BasicParser( );
-//        Options options = new Options( );
-//        options.addOption("h", "help", false, "Print this usage information");
+        CommandLineParser parser = new BasicParser();
+        Options options = new Options();
+        options.addOption("h", "help", false, "Print this usage information");
 //        options.addOption("v", "verbose", false, "Print out VERBOSE information" );
-//        options.addOption("f", "file", true, "File to save program output to");
-        // Parse the program arguments
-//        CommandLine commandLine = parser.parse( options, args );
+        options.addOption("kjb", "kjb", true, "File to save program output to");
+        options.addOption("ktr", "ktr", true, "File to save program output to");
+//         Parse the program arguments
+        String ktrFilePath = "";
+        String kjbFilePath = "";
+        try {
+            CommandLine commandLine = parser.parse( options, args );
+            ktrFilePath = commandLine.getOptionValue("ktr");
+            kjbFilePath = commandLine.getOptionValue("kjb");
 
-        System.out.println("eeee");
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
 
 
-//        System.out.println("start------------------------");
-//        App.runKettleTransfer("/Users/fenghuacaijing/Documents/test_news.ktr");
-//        System.out.println("end one ----------");
+        if (ktrFilePath!=null){
+            System.out.println("[执行转换]-------"+ktrFilePath+"--------");
+            App.runKettleTransfer(ktrFilePath);
+            System.out.println("[执行转换完成]-----------------------------]");
+        }
+
+        if(kjbFilePath!=null){
+            System.out.println("[执行作业]-------"+kjbFilePath+"--------");
+            App.runJob(kjbFilePath);
+            System.out.println("[执行作业完成]-----------------------------]");
+        }
+
 
 
         System.out.println("end");
     }
 
+    /**
+     * 执行KETTLE转换
+     * @param ktrFilePath
+     * @return
+     */
     public static boolean runKettleTransfer( String ktrFilePath) {
 
         Trans trans = null;
@@ -87,6 +107,28 @@ public class App
         } catch (Exception e) {
             logger_error.error("ExecKettleUtil@runKettleTransfer:"+uuid, e);
             return false;
+        }
+    }
+
+
+    /**
+     * 执行KETTLE作业
+     * @param jobPath
+     */
+    public static void runJob(String jobPath) {
+        try {
+            KettleEnvironment.init();
+            // jobname 是Job脚本的路径及名称
+            JobMeta jobMeta = new JobMeta(jobPath, null);
+            Job job = new Job(null, jobMeta);
+            job.start();
+            job.waitUntilFinished();
+            if (job.getErrors() > 0) {
+                throw new Exception(
+                        "There are errors during job exception!(执行job发生异常)");
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
         }
     }
 }
